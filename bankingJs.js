7cdbf2e1bfcf221baeca426063aa4e666c034f5a
Al-AminParevs <contact.alaminparves@gmail.com>
2022-03-13 15:14:23 +0600

    final commit

--- bankingJs.js
+++ bankingJs.js
@@ -10,18 +10,25 @@ function getInputValue(fieldId){
     inputField.value='';
     return  value;
 }
+function getInnerTextValue(fieldId){
+    const fieldTag=document.getElementById(fieldId);
+    const fieldValueInText=fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
 
 //Update totall
 
 function updateTotal(fieldId,amount){
-    const totalTag =document.getElementById(fieldId);
-    //that value is now in the blance,its use like text
-    const previousTotalInText =totalTag.innerText;
-    //covert text to float
-    const previousTotal=parseFloat(previousTotalInText);
+    // const totalTag =document.getElementById(fieldId);
+    // //that value is now in the blance,its use like text
+    // const previousTotalInText =totalTag.innerText;
+    // //covert text to float
+    // const previousTotal=parseFloat(previousTotalInText);
+    const previousTotal = getInnerTextValue(fieldId);
     const newTotal = previousTotal+amount;
     //Set new total in input text, that is text so we use inner text.
-    totalTag.innerText=newTotal;
+    document.getElementById(fieldId).innerText=newTotal;
     return newTotal;
 }
 
@@ -29,9 +36,10 @@ function updateTotal(fieldId,amount){
 //update balance
 
 function updateBalance(amount, isAdd){
-    const balanceTag=document.getElementById('balance-total');
-    const balanceInText=balanceTag.innerText;
-    const previousBalance=parseFloat(balanceInText);
+    // const balanceTag=document.getElementById('balance-total');
+    // const balanceInText=balanceTag.innerText;
+    // const previousBalance=parseFloat(balanceInText);
+    const previousBalance=getInnerTextValue('balance-total');
     let newBalance;
     if(isAdd == true){
         newBalance=previousBalance + amount;
@@ -39,21 +47,28 @@ function updateBalance(amount, isAdd){
     else{
         newBalance=previousBalance - amount;
     }
-    balanceTag.innerText=newBalance;
+    document.getElementById('balance-total').innerText=newBalance;
 }
 
 document.getElementById('deposit-button').addEventListener('click',function(){
    const amount= getInputValue('deposit-input');
-   updateTotal('deposit-total',amount);
-   updateBalance(amount,true);
+   if(amount>0){
+    updateTotal('deposit-total',amount);
+    updateBalance(amount,true);
+   }
+   
 
 });
 
 //Handle withdraw
 document.getElementById('withdraw-button').addEventListener('click',function(){
     const amount= getInputValue('withdraw-input');
-    updateTotal('withdraw-total',amount);
-    updateBalance(amount,false);
+    const balance=getInnerTextValue('balance-total');
+    if(amount>0 && amount<=balance){
+        updateTotal('withdraw-total',amount);
+        updateBalance(amount,false);
+    }
+    
  });
 
 
